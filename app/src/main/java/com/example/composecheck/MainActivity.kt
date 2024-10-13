a0c8139f301c603b9443b42363831eba5f8b8643
Abdul Rahim <89729367+AbdulRahim908@users.noreply.github.com>
2024-10-14 01:08:06 +0500

    second commit

--- app/src/main/java/com/example/composecheck/MainActivity.kt
+++ app/src/main/java/com/example/composecheck/MainActivity.kt
@@ -18,24 +18,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composecheck.ui.theme.ComposeCheckTheme
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController= rememberNavController()
-            NavHost(navController = navController, startDestination ="screen_A" , builder = {
-                composable("screen_A"){
-                    ScreenA(navController)
-                }
-                composable("screen_B"){
-                    ScreenB()
-                }
-            })
-//            ScreenA()
+//            val navController= rememberNavController()
+//            NavHost(navController = navController, startDestination ="screen_A" , builder = {
+//                composable("screen_A"){
+//                    ScreenA(navController)
+//                }
+//                composable("screen_B"){
+//                    ScreenB()
+//                }
+//            })
+            ScreenA()
 
 
 
--- app/src/main/java/com/example/composecheck/Screen1.kt
+++ app/src/main/java/com/example/composecheck/Screen1.kt
@@ -1,30 +1,75 @@
 package com.example.composecheck
-
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.fillMaxWidth
+
+//import androidx.navigation.NavController
 
 @Composable
 fun ScreenA(
-    navController:NavController
+//    navController:NavController
 ) {
+    MyAppBar()
     Column(
         Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Screen 1")
-        Button(onClick = {navController.navigate("screen_B")}
+        Button(onClick = {""}
+//        {navController.navigate("screen_B")}
 
         ) {
             Text(text = "Go To Screen 2")
         }
     }
 
+}
+@Composable
+fun MyAppBar() {
+    TopAppBar(
+//        windowInsets = AppBarDefaults.topAppBarWindowInsets,
+        title = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 56.dp), // Adjust padding to prevent overlap with actions
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Categories")
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        },
+        actions = {
+            // RowScope here, so these icons will be placed horizontally
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.ShoppingCart, contentDescription = "Localized description")
+            }
+
+        }
+    )
 }
